e1b8df0845d406d1c63ab0352149dfbb2aa0adfc
Mark Troyer <mtroyer@zuora.com>
2017-10-24 17:42:31 -0700

    Update sphinx configs in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import find_packages, setup
-from sphinx.setup_command import BuildDoc
 
-cmdclass = {'build_sphinx': BuildDoc}
 name = 'ZenossAPIClient'
 version = '0.1'
 release = '0.1.0'
@@ -16,14 +14,6 @@ setup(name=name,
       packages=find_packages(where='src'),
       package_dir={'': 'src'},
       zip_safe=False,
-      cmdclass=cmdclass,
-      command_options={
-          'build_sphinx': {
-              'project': ('setup.py', name),
-              'version': ('setup.py', version),
-              'release': ('setup.py', release),
-          }
-      },
       setup_requires=[
           'pytest-runner>=2.12.1',
           'Sphinx>=1.6.4',
