303ed8d5e185908d51346108a1658737a9f3999f
Mark Troyer <mtroyer@zuora.com>
2017-10-25 09:36:22 -0700

    add meta data to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,30 @@ release = '0.1.0'
 setup(name=name,
       version=release,
       description='Zenoss API client module',
+      long_description="""
+Python module for interacting with the Zenoss API an an object-oriented way.
+The philosophy here is to use objects to work with everything in the Zenoss 
+API, and to try to normalize the various calls to the different routers.
+Thus `get` methods will always return an object, `list` methods will return 
+data. All methods to add or create start with `add`, all remove or delete 
+start with `delete`. As much as possible the methods try to hide the 
+idiosyncrasies of the JSON API, and to do the work for you, for example 
+by letting you use a device name instead of having to provide the full 
+device UID for every call.
+""",
       url='https://github.com/Zuora-TechOps/ZenossAPIClient',
       author='Mark Troyer',
       author_email='disco@zuora.com',
       license='Apache',
+      classifiers=[
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: System :: Monitoring',
+      ],
+      keywords='Zenoss monitoring api',
       packages=find_packages(where='src'),
       package_dir={'': 'src'},
       zip_safe=False,
