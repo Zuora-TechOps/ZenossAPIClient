4f73a4b9f27a023bb91631d3524208a5903e8a29
Mark Troyer <mtroyer@zuora.com>
2017-11-15 11:17:35 -0800

    monitor router tests

---
+++ tests/routers/monitor_resp.py
@@ -0,0 +1,114 @@
+tree = {
+    "uuid": "b2781216-19ca-46d6-b4dc-2dbdf75e3909",
+    "action": "MonitorRouter",
+    "result": [
+        {
+            "leaf": False,
+            "name": "localhost",
+            "text": "localhost",
+            "id": ".zport.dmd.Monitors.Hub.localhost",
+            "devcount": 0,
+            "path": "/zport/dmd/Monitors/Hub/localhost",
+            "ccbacked": False,
+            "type": "hub",
+            "children": [
+                {
+                    "ccbacked": True,
+                    "leaf": False,
+                    "name": "localhost",
+                    "text": "localhost",
+                    "children": [],
+                    "devcount": 0,
+                    "path": "/zport/dmd/Monitors/Performance/localhost",
+                    "type": "collector",
+                    "id": ".zport.dmd.Monitors.Performance.localhost",
+                    "uid": "/zport/dmd/Monitors/Performance/localhost"
+                }
+            ],
+            "uid": "/zport/dmd/Monitors/Hub/localhost"
+        },
+        {
+            "leaf": False,
+            "name": "testhub",
+            "text": "testhub",
+            "id": ".zport.dmd.Monitors.Hub.testhub",
+            "devcount": 0,
+            "path": "/zport/dmd/Monitors/Hub/testhub",
+            "ccbacked": False,
+            "type": "hub",
+            "children": [
+                {
+                    "ccbacked": True,
+                    "leaf": False,
+                    "name": "testcollector",
+                    "text": "testcollector",
+                    "children": [],
+                    "devcount": 179,
+                    "path": "/zport/dmd/Monitors/Performance/testcollector",
+                    "type": "collector",
+                    "id": ".zport.dmd.Monitors.Performance.testcollector",
+                    "uid": "/zport/dmd/Monitors/Performance/testcollector"
+                }
+            ],
+            "uid": "/zport/dmd/Monitors/Hub/testhub"
+        }
+    ],
+    "tid": 1,
+    "type": "rpc",
+    "method": "getTree"
+}
+
+collector = {
+    "uuid": "0de03747-255e-4efb-ac1b-d4b59dc3d277",
+    "action": "MonitorRouter",
+    "result": {
+        "data": {
+            "ccbacked": True,
+            "leaf": False,
+            "name": "testcollector",
+            "text": "testcollector",
+            "children": [],
+            "devcount": 179,
+            "path": "/zport/dmd/Monitors/Performance/testcollector",
+            "type": "collector",
+            "id": ".zport.dmd.Monitors.Performance.testcollector",
+            "uid": "/zport/dmd/Monitors/Performance/testcollector"
+        },
+        "success": True
+    },
+    "tid": 1,
+    "type": "rpc",
+    "method": "getInfo"
+}
+
+params = {
+    "uuid": "418de426-356d-4535-b679-3ac4ac4360c4",
+    "action": "MonitorRouter",
+    "result": {
+        "data": {
+            "configCycleInterval": 360,
+            "pingCycleInterval": 60,
+            "discoveryNetworks": "",
+            "description": "",
+            "modelerCycleInterval": 720,
+            "processCycleInterval": 180,
+            "meta_type": "PerformanceConf",
+            "wmiqueryTimeout": 100,
+            "statusCycleInterval": 60,
+            "eventlogCycleInterval": 60,
+            "name": "testcollector",
+            "wmibatchSize": 10,
+            "pingTimeOut": 1.5,
+            "winCycleInterval": 60,
+            "pingTries": 2,
+            "inspector_type": "PerformanceConf",
+            "zenProcessParallelJobs": 10,
+            "id": "testcollector",
+            "uid": "/zport/dmd/Monitors/Performance/testcollector"
+        },
+        "success": True
+    },
+    "tid": 1,
+    "type": "rpc",
+    "method": "getCollector"
+}
---
+++ tests/routers/test_monitor_router.py
@@ -0,0 +1,162 @@
+import json
+import pytest
+from zenossapi.apiclient import ZenossAPIClientError
+from zenossapi.routers.monitor import MonitorRouter, ZenossHub, ZenossCollector
+import monitor_resp
+
+
+pytest_plugins = "pytest-responses"
+url = 'https://zenoss/zport/dmd'
+headers = dict(
+    ContentType='application/json'
+)
+resp_body_template = dict(
+    result=dict(
+        success=True,
+        data=[],
+    )
+)
+
+
+def request_callback(request):
+    rdata = json.loads(request.body)
+    resp_headers = dict(ContentType='application/json')
+
+    def getTree(rdata):
+        return monitor_resp.tree
+
+    def getInfo(rdata):
+        return monitor_resp.collector
+
+    def getCollector(rdata):
+        return monitor_resp.params
+
+    method = locals()[rdata['method']]
+    resp_body = method(rdata['data'][0])
+
+    return (200, resp_headers, json.dumps(resp_body))
+
+
+def responses_callback(responses):
+    responses.add_callback(
+        responses.POST,
+        '{0}/monitor_router'.format(url),
+        callback=request_callback,
+        content_type='application/json',
+    )
+
+
+class TestMonitorRouter(object):
+
+    def test_monitor_router_init(self):
+        mr = MonitorRouter(url, headers, False)
+        assert mr.id is None
+
+    def test_monitor_router_tree(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.tree()
+        assert len(resp) == 2
+        assert len(resp[0]['children']) == 1
+        assert resp[0]['name'] == "localhost"
+        assert resp[1]['name'] == "testhub"
+
+    def test_monitor_router_list_hubs(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.list_hubs()
+        assert len(resp) == 2
+        assert resp[0]['name'] == "localhost"
+        assert resp[1]['name'] == "testhub"
+        assert resp[0]['uid'] == "Monitors/Hub/localhost"
+        assert resp[1]['uid'] == "Monitors/Hub/testhub"
+
+    def test_monitor_router_list_collectors(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.list_collectors()
+        assert len(resp) == 2
+        assert resp[0]['name'] == "localhost"
+        assert resp[0]['devcount'] == 0
+        assert resp[0]['hub'] == "localhost"
+        assert resp[0]['uid'] == "Monitors/Performance/localhost"
+        assert resp[1]['name'] == "testcollector"
+        assert resp[1]['devcount'] == 179
+        assert resp[1]['hub'] == "testhub"
+        assert resp[1]['uid'] == "Monitors/Performance/testcollector"
+
+    def test_monitor_router_get_hubs(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.get_hubs()
+        assert len(resp) == 2
+        assert isinstance(resp[0], ZenossHub)
+        assert isinstance(resp[1], ZenossHub)
+
+    def test_monitor_router_get_hub(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.get_hub('testhub')
+        assert isinstance(resp, ZenossHub)
+        assert resp.name == "testhub"
+        assert resp.devcount == 0
+        assert len(resp.collectors) == 1
+
+    def test_monitor_router_get_bad_hub(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        resp = mr.get_hub('badhub')
+        assert resp is None
+
+    def test_monitor_router_zenosshub_get_collectors(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        zh = mr.get_hub('testhub')
+        resp = zh.get_collectors()
+        assert len(resp) == 1
+        assert isinstance(resp[0], ZenossCollector)
+        assert resp[0].name == "testcollector"
+
+    def test_monitor_router_zenosshub_get_collector(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        zh = mr.get_hub('testhub')
+        resp = zh.get_collector('testcollector')
+        assert isinstance(resp, ZenossCollector)
+        assert resp.name == "testcollector"
+        assert resp.devcount == 179
+        assert resp.text == "testcollector"
+        assert resp.path == "/zport/dmd/Monitors/Performance/testcollector"
+        assert resp.id == ".zport.dmd.Monitors.Performance.testcollector"
+        assert resp.configCycleInterval == 360
+        assert resp.pingCycleInterval == 60
+        assert resp.discoveryNetworks == ""
+        assert resp.description == ""
+        assert resp.modelerCycleInterval == 720
+        assert resp.processCycleInterval == 180
+        assert resp.meta_type == "PerformanceConf"
+        assert resp.wmiqueryTimeout == 100
+        assert resp.statusCycleInterval == 60
+        assert resp.eventlogCycleInterval == 60
+        assert resp.wmibatchSize == 10
+        assert resp.pingTimeOut == 1.5
+        assert resp.winCycleInterval == 60
+        assert resp.pingTries == 2
+        assert resp.inspector_type == "PerformanceConf"
+        assert resp.zenProcessParallelJobs == 10
+
+    def test_monitor_router_zenosshub_get_bad_collector(self, responses):
+        responses_callback(responses)
+
+        mr = MonitorRouter(url, headers, False)
+        zh = mr.get_hub('testhub')
+        resp = zh.get_collector('badcollector')
+        assert resp is None
